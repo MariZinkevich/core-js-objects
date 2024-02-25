c16741641baf4ace165c5b88f311e0e1870ebd15
Mary Zin <mary22091991mary@gmail.com>
2024-02-25 13:08:47 +0300

    feat: solution mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -35,8 +35,21 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  let concatObj = [];
+  const res = {};
+  for (let i = 0; i < objects.length; i += 1) {
+    concatObj = concatObj.concat(Object.entries(objects[i]));
+  }
+  for (let i = 0; i < concatObj.length; i += 1) {
+    if (concatObj[i][0] in res) {
+      res[concatObj[i][0]] += concatObj[i][1];
+    } else {
+      const [key, value] = concatObj[i];
+      res[key] = value;
+    }
+  }
+  return res;
 }
 
 /**
